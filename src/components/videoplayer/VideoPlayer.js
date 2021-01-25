42ba0b9806453550423b8cce5383966cf3476287
Yassine Alikhbari <yalikhbari@altitudo.com>
2021-01-25 20:31:33 +0100

    Fix iframe attribute casing

--- src/components/videoplayer/VideoPlayer.js
+++ src/components/videoplayer/VideoPlayer.js
@@ -8,7 +8,7 @@ const VideoPlayer = (props) => {
         <iframe title={`www.youtube.com/${props.youtubeId}`} class="custom-iframes"
           src={`https://www.youtube.com/embed/${props.youtubeId}?wmode=transparent&autoplay=1&mute=1&theme=dark&controls=1&autohide=1&loop=1&showinfo=0&playlist=false&enablejsapi=0&rel=0&vq=hd1440`}
           frameBorder="0"
-          allowfullscreen
+          allowFullScreen
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
       </div>
     </div>
