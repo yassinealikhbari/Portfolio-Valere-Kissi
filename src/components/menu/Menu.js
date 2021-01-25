cbc41d223fe4e0008c254675caf4e72a9e960a5d
Yassine Alikhbari <yalikhbari@altitudo.com>
2021-01-25 20:01:08 +0100

    fix alignement

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -21,7 +21,7 @@ const Menu = () => {
                             <Nav.Link href="service">Service</Nav.Link>
                         </Nav.Item>
                         <Nav.Item>
-                            <Nav.Link href="contacts">Contact</Nav.Link>
+                            <Nav.Link href="contact">Contact</Nav.Link>
                         </Nav.Item>
                     </Nav>
                 </Navbar.Collapse>
