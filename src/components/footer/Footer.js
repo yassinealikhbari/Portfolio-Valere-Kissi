008892fae6a2f7f99ba3cb62aaf81327e00b8b99
Yassine Alikhbari <yalikhbari@altitudo.com>
2021-01-25 11:52:37 +0100

    Fix layout style

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,37 +3,42 @@ import './Footer.css';
 var FontAwesome = require('react-fontawesome');
 
 const Footer = () => {
+    var date = new Date();
+    var curretYear = date.getFullYear();
     return (
-        <div>
+        <div class="vk-flex-container bg-blk">
             <div id="vk-footer">
                 <a href="https://www.instagram.com/valere_4ever_young/" target="_blank">
-                <FontAwesome
-                    name='instagram'
-                    size='1x'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
-                /></a>
+                    <FontAwesome
+                        name='instagram'
+                        size='1x'
+                        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
+                    /></a>
 
                 <a href="https://www.facebook.com/valere.kissi.3/" target="_blank">
-                <FontAwesome
-                    name='facebook'
-                    size='1x'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
-                /></a>
+                    <FontAwesome
+                        name='facebook'
+                        size='1x'
+                        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
+                    /></a>
 
                 <a href="https://www.youtube.com/channel/UCsT3qb37M9pERgRUhLyFaUQ/" target="_blank">
-                <FontAwesome
-                    name='youtube'
-                    size='1x'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
-                /></a>
+                    <FontAwesome
+                        name='youtube'
+                        size='1x'
+                        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
+                    /></a>
 
                 <a href="https://www.linkedin.com/in/valerekissi/" target="_blank">
-                <FontAwesome
-                    name='linkedin'
-                    size='1x'
-                    style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
-                /></a>
+                    <FontAwesome
+                        name='linkedin'
+                        size='1x'
+                        style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)', margin: '0 20px' }}
+                    /></a>
 
+            </div>
+            <div class="copy-container">
+                ©2021-{curretYear} Valere Kissi.
             </div>
         </div>
     );
