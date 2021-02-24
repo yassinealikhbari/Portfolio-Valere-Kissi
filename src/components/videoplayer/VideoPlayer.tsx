65846d1ecfd70fc9cfe406e5e5c06822fe81f5d4
Yassine Alikhbari <yassine.alikhbari@hotmail.it>
2021-02-24 23:49:18 +0100

    Remove playlist parameter from youtube embed video

--- src/components/videoplayer/VideoPlayer.tsx
+++ src/components/videoplayer/VideoPlayer.tsx
@@ -18,7 +18,7 @@ export class VideoPlayer extends React.Component<IVideoPlayerProps, IVideoPlayer
       <div className="responsive-container">
         <div className="embed-responsive embed-responsive-21by9">
           <iframe title={`www.youtube.com/${this.props.youtubeId}`} className="custom-iframes"
-            src={`https://www.youtube.com/embed/${this.props.youtubeId}?wmode=transparent&autoplay=1&mute=1&theme=dark&controls=1&autohide=1&loop=1&showinfo=0&playlist=false&enablejsapi=0&rel=0&vq=hd1440`}
+            src={`https://www.youtube.com/embed/${this.props.youtubeId}?wmode=transparent&autoplay=1&mute=1&theme=dark&controls=1&autohide=1&loop=1&showinfo=0&enablejsapi=0&rel=0&vq=hd1440`}
             frameBorder="0"
             allowFullScreen
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
