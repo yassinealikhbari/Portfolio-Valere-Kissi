5905536729b59c623ee5ee2fcaf05a85610ae800
Yassine Alikhbari <yassine.alikhbari@hotmail.it>
2021-02-24 22:21:38 +0100

    Add email notification

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import emailjs from 'emailjs-com';
-import './Contacts.css'
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 import { Form, Button } from 'react-bootstrap';
+import $ from 'jquery';
+import './Contacts.css'
 
 export interface IContactsProps {
 }
 
 export interface IContactsState {
+  openError: boolean;
+  openSuccess: boolean;
 }
 
 export class Contacts extends React.Component<IContactsProps, IContactsState> {
@@ -17,23 +22,29 @@ export class Contacts extends React.Component<IContactsProps, IContactsState> {
   constructor(props: IContactsProps) {
     super(props);
 
-    this.state = {};
-
-  }
-
-  private sendEmail = (e: any) => {
-    e.preventDefault();
-    emailjs.sendForm(this.serviceId, this.templateId, e.target, this.userId)
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
+    this.state = {
+      openError: false,
+      openSuccess: false
+    };
   }
 
   public render() {
     return (
       <div id="contact-container" className="vk-flex-container">
+
+        <Snackbar open={this.state.openSuccess} autoHideDuration={6000} onClose={this.handleCloseSuccess}>
+          <MuiAlert onClose={this.handleCloseSuccess} severity="success">
+            Thank you for contacting me – I will get back to you soon!
+            </MuiAlert>
+        </Snackbar>
+
+
+        <Snackbar open={this.state.openError} autoHideDuration={20000} onClose={this.handleCloseError}>
+          <MuiAlert onClose={this.handleCloseError} severity="error">
+            Error while trying to send this message, please try later or contact me here: <a style={{color: 'rgb(97, 26, 21)'}} href = "mailto:valerekissi69@gmail.com">valerekissi69@gmail.com</a>
+            </MuiAlert>
+        </Snackbar>
+
         <div id="contact-inner-container">
           <Form onSubmit={this.sendEmail}>
             <Form.Group controlId="user_name">
@@ -53,11 +64,60 @@ export class Contacts extends React.Component<IContactsProps, IContactsState> {
 
             <Button variant="dark" type="submit" className="float-right">
               Send
-        </Button>
+              </Button>
 
           </Form>
         </div>
       </div>
     );
   };
+  
+
+  private sendEmail = (e: any) => {
+    e.preventDefault();
+    emailjs.sendForm(this.serviceId, this.templateId, e.target, this.userId)
+      .then((result) => {
+        this.resetForm();
+        this.openSnackbarSuccess();
+      }, (error) => {
+        this.openSnackbarError();
+      });
+  }
+
+  private resetForm = () => {
+    $("#user_name").val('');
+    $("#user_email").val('');
+    $("#message").val('');
+  }
+
+  private openSnackbarSuccess = () => this.setState({ openSuccess: true });
+  private openSnackbarError = () => this.setState({ openError: true });
+
+  private handleCloseSuccess = (event: any) => {
+    debugger;
+    if (event === 'clickaway') {
+      return;
+    }
+
+    this.setState({ openSuccess: false });
+
+  };
+
+  private handleCloseError = (event: any) => {
+    debugger;
+    if (event === 'clickaway') {
+      return;
+    }
+    this.setState({ openError: false });
+  };
+
+
+  // let useStyles = makeStyles((theme) => ({
+  //   root: {
+  //     width: '100%',
+  //     '& > * + *': {
+  //       marginTop: theme.spacing(2),
+  //     },
+  //   },
+  // }));
 }
